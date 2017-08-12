49894f5e892909478cdcd77eb430ab268381466b
Frankaiyou <aiyoupass@gmail.com>
2017-08-12 19:45:55 +0800

    Update FastPower.cpp

--- math/FastPower.cpp
+++ math/FastPower.cpp
@@ -3,8 +3,8 @@ T Pow(T a,T b,T c){
     int ans=1,base=a;
     while(b){
     	if(b&1)
-			ans=(ans%c*base%c)%c;
-		base=(base%c*base%c)%c;
+			ans=(ans*base)%c;
+		base=(base*base)%c;
 		b>>=1;
 	}
 	return ans%c;
