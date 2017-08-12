2c1bfbfceee17a9d7eb6a7867890d9727664f529
Frankaiyou <aiyoupass@gmail.com>
2017-08-12 19:08:34 +0800

    Update exgcd.cpp

--- math/exgcd.cpp
+++ math/exgcd.cpp
@@ -1,9 +1,21 @@
-inline void exgcd(int a,int b,int &x,int &y)
-{
-	if(b==0)
-	{
+template<typename T>
+inline void Exgcd(T a,T b,T &x,T &y){
+	if(b==0){
 		x=1,y=0;return;
 	}
-	exgcd(b,a%b,x,y);
+	Exgcd(b,a%b,x,y);
 	int t=x;x=y;y=t-a/b*y;
+}
+template<typename T>
+T exgcd(T x,T z,T p){
+    T t=Gcd(x,p);
+    p=-p;
+    if(z%t)
+        return -1;
+    x/=t;z/=t;p/=t;
+    T a,b;
+    Exgcd(x,p,a,b);
+    a=(ll)a*z%p;
+    while(a<0)a+=p;
+    return a;
 }
