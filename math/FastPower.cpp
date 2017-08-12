dac23f417d0e3dcc391d3e489e5a8a3dc330203c
MingqiHuang <hmq011212@163.com>
2017-08-12 19:27:59 +0800

    Revert "Frankaiyou patch 1"

--- math/FastPower.cpp
+++ math/FastPower.cpp
@@ -1,11 +1,12 @@
-template<typename T> 
-T Pow(T a,T b,T c){
-    int ans=1,base=a;
-    while(b){
-    	if(b&1)
-			ans=(ans%c*base%c)%c;
-		base=(base%c*base%c)%c;
-		b>>=1;
-	}
-	return ans%c;
+inline int FastPower(int Tp,int Up,int mod)
+{
+    int r=1,base=Tp;
+    while(b)
+    {
+        if(b&1)
+            r*=base;
+        base*=base;
+        b>>1;
+    }
+    return r;
 }
