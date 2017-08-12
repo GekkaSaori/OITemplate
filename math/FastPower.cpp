4d49d9d1f765dd5858603b11e7eb5e01759eb746
Frankaiyou <aiyoupass@gmail.com>
2017-08-12 19:03:56 +0800

    Update FastPower.cpp

--- math/FastPower.cpp
+++ math/FastPower.cpp
@@ -1,12 +1,11 @@
-inline int FastPower(int Tp,int Up,int mod)
-{
-    int r=1,base=Tp;
-    while(b)
-    {
-        if(b&1)
-            r*=base;
-        base*=base;
-        b>>1;
-    }
-    return r;
+template<typename T> 
+T Pow(T a,T b,T c){
+    int ans=1,base=a;
+    while(b){
+    	if(b&1)
+			ans=(ans%c*base%c)%c;
+		base=(base%c*base%c)%c;
+		b>>=1;
+	}
+	return ans%c;
 }
